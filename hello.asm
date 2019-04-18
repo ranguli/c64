e87e6c9a0dd19d3b5ede8e59f61866901c979f45
ranguli <hello@joshmurphy.ca>
2019-04-18 14:47:42 -0230

    Fixed whitespace

--- hello.asm
+++ hello.asm
@@ -1,18 +1,18 @@
 ; hello world for the C64
-	
-    *=$0801    			; Tells the compiler that our code should be located at
+
+	*=$0801				; Tells the compiler that our code should be located at
 						; this hex value
 
-	jsr $e544  			; KERNAL function (a utility function built into the 
+	jsr $e544			; KERNAL function (a utility function built into the
 						; C64 ROM) at $e544 which clears the screen
 
-	lda #$F1  			; Load hex number 3 into the accumulator register
-	sta $d020  			; Store hex number 3 in memory location $d020, which 
+	lda #$F1			; Load hex number 3 into the accumulator register
+	sta $d020			; Store hex number 3 in memory location $d020, which
 						; controls background color.
 
-	sta $d021  			; Same thing, but the border color.
+	sta $d021			; Same thing, but the border color.
 
-	ldx #$00   			; Load 0 into the X index register. X and Y index 
+	ldx #$00			; Load 0 into the X index register. X and Y index
 						; registers are used for indexing, looping, and indexed
 						; addressing.
 
@@ -32,6 +32,6 @@ wait:					; Another label for a memory location
 						; loop
 
 text:					; Memory location label 'text'
-	!scr "              hello world               " ; special compiler keyword that converts text
+	!scr "             hello world               " ; special compiler keyword that converts text
 						; into format understood by the C64. the space padding makes it appear
 						; in the center of the screen
